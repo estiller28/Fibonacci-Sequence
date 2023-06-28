269745f896604b61e89d5334188c89d7a92fa146
Daniel Estiller <estillerdaniel@gmail.com>
2023-06-28 11:51:42 +0800

    Added else block to execute the fibonacci sequence

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 function getFibonacciSequence($num){
     if($num == 0 || $num > 20){
         echo "Please input a number from 1 to 20!";
+    }else{
+        $fib = [0,1];
+
+        for($i = 2; $i < $num; $i++){
+            $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
+        }
+
+        return $fib;
     }
-
-    $fib = array(0, 1);
-
-    for($i = 2; $i < $num; $i++){
-        $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
-    }
-
-    return $fib;
 }
 
 $testCase = getFibonacciSequence(5);
